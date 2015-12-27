efc96201d5682dc373889d814f41364b627be218
Erik Tittel <et23@live.de>
2015-12-27 20:33:27 +0100

    more tests added

--- checkout/step_definitions/CheckoutSteps.java
+++ checkout/step_definitions/CheckoutSteps.java
@@ -8,20 +8,23 @@ import implementation.Checkout;
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class CheckoutSteps {
 
-  int bananaPrice = 0;
-  Checkout checkout;
+  private int bananaPrice = 0;
+  private Checkout checkout = new Checkout();
+  private Map<String, Integer> prices = new HashMap<>();
 
   @Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
   public void thePriceOfAIsC(String name, int price) throws Throwable {
-    bananaPrice = price;
+	prices.put(name, price);
   }
 
   @When("^I checkout (\\d+) \"([^\"]*)\"$")
   public void iCheckout(int itemCount, String itemName) throws Throwable {
-    checkout = new Checkout();
-	checkout.add(itemCount, bananaPrice);
+	checkout.add(itemCount, prices.get(itemName));
   }
 
   @Then("^the total price should be (\\d+)c$")

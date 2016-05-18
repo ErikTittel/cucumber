40b0be70efe88efc70fda16c71a3d91292d1d8e9
Erik Tittel <et23@live.de>
2016-05-18 20:47:04 +0200

    added AtmServlet with correct case of the file name

---
+++ src/main/java/nicebank/AtmServlet.java
@@ -0,0 +1,26 @@
+package nicebank;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AtmServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println(
+                "<html><head><title>Nice Bank ATM</title></head>"
+                + "<body><h1>Welcome to our nice bank!</h1>" +
+                        "<form action=\"/withdraw\" method=\"post\">" +
+                        "<label for=\"amount\"Amount</label>" +
+                        "<input type=\"text\" id=\"amount\" name=\"amount\">" +
+                        "<button type=\"submit\" id=\"withdraw\">Withdraw</button>" +
+                        "</form></body></html>"
+        );
+    }
+}
